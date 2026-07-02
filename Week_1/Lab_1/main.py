46ca2106a64251470627c20c367a5cfe4cffcccd
Caden Cramsey <cadencramsey@gmail.com>
2026-07-02 08:03:34 -0500

    Lab One

---
+++ Week_1/Lab_1/main.py
@@ -0,0 +1,22 @@
+'''
+Set up your {Google Colab} workspace, and in the main.py, using the print statement to display your student information and the class logistics, including:
+1. Your full name
+2. Your student Id
+3. Your email address
+4. Three discussed the policies of our class
+'''
+name = "Caden Cramsey"
+student_id = "20750632"
+email = "cadencramsey@gmail.com"
+
+policy_one = "Participation: It is a student’s responsibility to drop himself/herself from the class. Simply being absent from all the class activities will result in a final grade of F."
+
+policy_two = "Emailing Your Instructor: The subject line MUST include the course number, and the name of the student as follows “CS 03A: Jane Doe, Question regarding List”. Following these directives enables the instructor to identify quickly the student and course, facilitate a timely response, and avoid students’ emails from being filtered out."
+
+policy_three = "Late Submission : Late submission results in 15% penalty per week and up to 1 week, except with proof of medical or legal emergency. Each assignment has a due date and a cut-off date; once the cut-off date is passed, you will no longer be able to submit your assignment. Please make sure that you submit your assignment before the cut-off date."
+
+print("Hi, my name is " + name + " and my student ID is " + student_id + ". My email address is " + email + ".")
+print("Here are three policies of our class: ")
+print("1. " + policy_one)
+print("2. " + policy_two)
+print("3. " + policy_three)
